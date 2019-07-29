f4859b5377f30699ca9c5a2ff7ffe1fa1036012e
akouryy <akouryy7@gmail.com>
2019-07-30 01:11:29 +0900

    Calculate edge score (moutain) in KindaiScorer

--- src/main/scala/net/akouryy/kreins/scorer/KindaiScorer.scala
+++ src/main/scala/net/akouryy/kreins/scorer/KindaiScorer.scala
@@ -1,7 +1,7 @@
 package net.akouryy.kreins
 package scorer
 
-import game.Board
+import game.{Board, LightBoard}
 import util.InputUtil
 
 /**
@@ -11,7 +11,39 @@ final class KindaiScorer(val wbp: Int, val wcn: Int, val wfs: Int) extends Score
   def score(board: Board) = {
     CellScorer.score(board.unfixedBoard) * wbp +
       (board.possPlaceable.popcount - board.pass.possPlaceable.popcount) * wcn +
-      (board.fst.fixedRectangleCount - board.snd.fixedRectangleCount) * wfs
+      (board.fst.fixedRectangleCount - board.snd.fixedRectangleCount) * wfs +
+      mountDiff(board.toLightBoard) * wfs
+  }
+
+  @inline def mountDiff(board: LightBoard) = {
+    val f = board.fst
+    val s = board.snd
+
+    val a = {
+      val k = (f | s) & 0x0000000000000081L
+      if((~f & 0x000000000000007EL | k) == 0) 1
+      else if((~s & 0x000000000000007EL | k) == 0) -1
+      else 0
+    }
+    val b = {
+      val k = (f | s) & 0x8100000000000000L
+      if((~f & 0x7E00000000000000L | k) == 0) 1
+      else if((~s & 0x7E00000000000000L | k) == 0) -1
+      else 0
+    }
+    val c = {
+      val k = (f | s) & 0x0100000000000001L
+      if((~f & 0x0001010101010100L | k) == 0) 1
+      else if((~s & 0x0001010101010100L | k) == 0) -1
+      else 0
+    }
+    val d = {
+      val k = (f | s) & 0x8000000000000080L
+      if((~f & 0x0080808080808000L | k) == 0) 1
+      else if((~s & 0x0080808080808000L | k) == 0) -1
+      else 0
+    }
+    a + b + c + d
   }
 }
 
